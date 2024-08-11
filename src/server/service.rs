b9c60c1886ea114882c3426abc047a5c408b5251
Braden Everson <bradeneverson@gmail.com>
2024-08-11 13:06:40 -0500

    Impl Service for ServerService

--- src/server/service.rs
+++ src/server/service.rs
@@ -1,3 +1,12 @@
+use futures_util::Future;
+use http_body_util::Full;
+use hyper::{
+    body::{self, Bytes},
+    service::Service,
+};
+use hyper::{Request, Response};
+use serde::{Deserialize, Serialize};
+use std::pin::Pin;
 use tokio::sync::mpsc::UnboundedSender;
 use uuid::Uuid;
 
@@ -5,7 +14,25 @@ pub struct ServerService {
     sender: UnboundedSender<ServerMessage>,
 }
 
-pub enum ServerMessage {
+impl Service<Request<body::Incoming>> for ServerService {
+    type Response = Response<Full<Bytes>>;
+    type Error = hyper::http::Error;
+    type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>> + Send>>;
+
+    fn call(&self, req: Request<body::Incoming>) -> Self::Future {
+        todo!()
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct ServerMessage {
+    from: Option<Uuid>,
+    msg: MessageType,
+}
+
+#[derive(Serialize, Deserialize)]
+pub enum MessageType {
     ConnectReq(String),
-    Disconnect(Uuid),
+    Text(String),
+    Disconnect,
 }
