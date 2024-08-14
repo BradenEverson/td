8848147bf7cebe6e4e84169512dc9b6d989cc737
BradenEverson <bradeneverson@gmail.com>
2024-08-13 20:52:21 -0500

    Split streams and send writer over to state manager and keep read in internal loop to respond to async messages

--- src/server/service.rs
+++ src/server/service.rs
@@ -1,31 +1,43 @@
-use futures_util::Future;
+use futures::{executor::block_on, stream::SplitSink};
+use futures_util::{Future, StreamExt};
 use http_body_util::Full;
 use hyper::{
     body::{self, Bytes},
     service::Service,
+    upgrade::Upgraded,
 };
 use hyper::{Request, Response};
 use hyper_tungstenite::HyperWebsocket;
 use serde::{Deserialize, Serialize};
 use std::{ops::Deref, pin::Pin};
 use tokio::sync::mpsc::UnboundedSender;
+use tokio_tungstenite::{
+    tungstenite::{Message, WebSocket},
+    WebSocketStream,
+};
 use uuid::Uuid;
 
 pub struct ServerService {
-    sender: UnboundedSender<ServerMessage>,
+    pub sender: UnboundedSender<ServerMessage>,
 }
 
-pub type TokioMpscResult<T> = Result<T, tokio::sync::mpsc::error::SendError<ServerMessage>>;
+pub type TokioMpscError = tokio::sync::mpsc::error::SendError<ServerMessage>;
+pub type WebSocketReadStream =
+    SplitSink<WebSocketStream<hyper_util::rt::tokio::TokioIo<Upgraded>>, Message>;
 
 impl ServerService {
     pub fn new(tx: UnboundedSender<ServerMessage>) -> Self {
         Self { sender: tx }
     }
-    pub fn send_msg(&mut self, msg: ServerMessage) -> TokioMpscResult<()> {
+    pub fn send_msg(&mut self, msg: ServerMessage) -> Result<(), TokioMpscError> {
         self.sender.send(msg)
     }
-    pub fn chat(&mut self, from: Uuid, msg: &str) -> TokioMpscResult<()> {
+    pub fn chat(&mut self, from: Uuid, msg: &str) -> Result<(), TokioMpscError> {
         let message = ServerMessage::text(from, msg);
+        self.sender.send(message)
+    }
+    pub fn websocket(&mut self, socket: WebSocketReadStream) -> Result<(), TokioMpscError> {
+        let message = ServerMessage::new(None, MessageType::ConnectWs(socket));
         self.sender.send(message)
     }
 }
@@ -36,10 +48,34 @@ impl Service<Request<body::Incoming>> for ServerService {
     type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>> + Send>>;
 
     fn call(&self, mut req: Request<body::Incoming>) -> Self::Future {
+        let tx = self.sender.clone();
         if hyper_tungstenite::is_upgrade_request(&req) {
             // Upgrade to WebSocket
-            let (response, websocket) =
+            let (_, websocket) =
                 hyper_tungstenite::upgrade(&mut req, None).expect("Error upgrading to WebSocket");
+            let (writer, mut reader) = block_on(async { websocket.await.unwrap().split() });
+
+            let user_id = Uuid::new_v4();
+
+            tx.send(ServerMessage::new(
+                Some(user_id),
+                MessageType::ConnectWs(writer),
+            ))
+            .expect("Failed to send websocket write stream up channel");
+
+            tokio::spawn(async move {
+                while let Some(Ok(msg)) = reader.next().await {
+                    // TODO - Respond to websocket messages accordingly
+                    match msg {
+                        Message::Text(txt) => {
+                            tx.send(ServerMessage::text(user_id, &txt))?;
+                        }
+                        _ => {}
+                    }
+                }
+                Ok::<(), TokioMpscError>(())
+            });
+
             todo!();
         } else {
             // Traditional HTTP
@@ -55,11 +91,12 @@ pub struct ServerMessage {
 }
 
 impl ServerMessage {
+    fn new(from: Option<Uuid>, msg: MessageType) -> Self {
+        Self { from, msg }
+    }
+
     fn text(from: Uuid, msg: &str) -> Self {
-        Self {
-            from: Some(from),
-            msg: MessageType::Text(msg.into()),
-        }
+        Self::new(Some(from), MessageType::Text(msg.to_string()))
     }
 }
 
@@ -67,7 +104,7 @@ impl ServerMessage {
 pub enum MessageType {
     ConnectReq(String),
     Text(String),
-    ConnectWs(HyperWebsocket),
+    ConnectWs(WebSocketReadStream),
     Disconnect,
 }
 
